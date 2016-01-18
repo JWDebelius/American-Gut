4b7f0b76281998b893794797ad5891d07f37bde9
JWDebelius <j.debelius@gmail.com>
2016-01-18 15:21:18 -0800

    attempts to clean up ag_data

--- americangut/ag_data.py
+++ americangut/ag_data.py
@@ -8,10 +8,12 @@ import skbio
 
 class AgData:
     na_values = ['NA', 'unknown', '', 'no_data', 'None', 'Unknown']
-    alpha_columns = {'PD_whole_tree_1k', 'PD_whole_tree_10k',
-                     'shannon_1k', 'shannon_10k',
-                     'chao1_1k', 'chao1_10k',
-                     'observed_otus_1k', 'observed_otus_10k'}
+    alpha_columns = {'10k': ['PD_whole_tree_1k', 'PD_whole_tree_10k',
+                             'shannon_1k', 'shannon_10k',
+                             'chao1_1k', 'chao1_10k',
+                             'observed_otus_1k', 'observed_otus_10k'],
+                     '1k': ['PD_whole_tree_1k', 'shannon_1k', 'chao1_1k',
+                            'observed_otus_1k']}
     bodysite_limits = {'fecal': 55,
                        }
     subset_columns = {'IBD': 'SUBSET_IBD',
@@ -31,24 +33,19 @@ class AgData:
         if depth not in {'1k', '10k'}:
             raise ValueError('%s is not a supported rarefaction depth' % depth)
 
-        self.data_set = {'bodysite': bodysite,
-                         'sequence_trim': trim,
-                         'rarefaction_depth': depth,
-                         }
+        self.bodysite = bodysite
+        self.trim = trim
+        self.depth = depth
+        self.sub_participants = sub_participants
+        self.one_sample = one_sample
 
-        if one_sample:
-            self.data_set['samples_per_participants'] = 'one_sample'
-        else:
-            self.data_set['samples_per_participants'] = 'all_samples'
-
-        if sub_participants:
-            self.data_set['participant_set'] = 'sub'
-        else:
-            self.data_set['participant_set'] = 'all'
+        self._check_dataset()
 
         # Gets the base directory
-        base_dir = os.path.abspath('../primary-processing/agp_processing/'
-                                   '11-packaged')
+        base_dir = kwargs.get('base_dir',
+                              os.path.abspath('../primary-processing/'
+                                              'agp_processing/'
+                                              '11-packaged'))
 
         # Gets the data directory
         self.data_dir = os.path.join(base_dir,
@@ -64,7 +61,25 @@ class AgData:
 
         self.reload_files()
 
+    def _check_dataset(self):
+        self.data_set = {'bodysite': self.bodysite,
+                         'sequence_trim': self.trim,
+                         'rarefaction_depth': self.depth,
+                         }
+
+        if self.one_sample:
+            self.data_set['samples_per_participants'] = 'one_sample'
+        else:
+            self.data_set['samples_per_participants'] = 'all_samples'
+
+        if self.sub_participants:
+            self.data_set['participant_set'] = 'sub'
+        else:
+            self.data_set['participant_set'] = 'all'
+
     def reload_files(self):
+        self._check_dataset()
+
         # Determines the filepaths
         map_fp = os.path.join(self.data_dir,
                               'ag_%(rarefaction_depth)s_%(bodysite)s.txt'
@@ -86,10 +101,10 @@ class AgData:
                                 dtype=str,
                                 na_values=self.na_values)
         self.map_.set_index('#SampleID', inplace=True)
-        self.map_[list(self.alpha_columns)] = \
-            self.map_[list(self.alpha_columns)].astype(float)
+        columns = self.alpha_columns[self.data_set['rarefaction_depth']]
+        self.map_[columns] = self.map_[columns].astype(float)
 
-        self.otu = biom.load_table(otu_fp)
+        self.otu_ = biom.load_table(otu_fp)
 
         self.beta = {'unweighted_unifrac':
                      skbio.DistanceMatrix.read(unweighted_fp),
@@ -101,7 +116,7 @@ class AgData:
         self.outliers = self.map_.loc[(self.map_[metric] >= limit)].index
         keep = self.map_.loc[(self.map_[metric] < limit)].index
         self.map_ = self.map_.loc[keep]
-        self.otu = self.otu.filter(keep)
+        self.otu_ = self.otu_.filter(keep)
         self.beta = {'unweighted_unifrac':
                      self.beta['unweighted_unifrac'].filter(keep),
                      'weighted_unifrac':
@@ -119,13 +134,13 @@ class AgData:
     def cast_subset(self):
         pass
 
-    def return_dataset(self, group):
-        """..."""
-        defined = self.map_[~ pd.isnull(self.map_[group.name])].index
-        map_ = self.map_.loc[defined]
-        otu_ = self.otu.filter(defined, axis='sample')
-        beta = {k: v.filter(defined) for k, v in self.beta.iteritems()}
-        return map_, otu_, beta
+    # def return_dataset(self, group):
+    #     """..."""
+    #     defined = self.map_[~ pd.isnull(self.map_[group.name])].index
+    #     map_ = self.map_.loc[defined]
+    #     otu_ = self.otu_.filter(defined, axis='sample')
+    #     beta = {k: v.filter(defined) for k, v in self.beta.iteritems()}
+    #     return map_, otu_, beta
 
     def clean_up_column(self, group):
         """Cleans up the indicated mapping column using the group"""
--- tests/test_ag_data.py
+++ tests/test_ag_data.py
@@ -1,36 +1,205 @@
-import pandas as pd
-import numpy as np
+import os
+from unittest import TestCase, main
 
+import biom
+import numpy as np
+import pandas as pd
 import numpy.testing as npt
 import pandas.util.testing as pdt
 
-from testfixtures import TempDirectory
-from unittest import TestCase, main
-
+import americangut as ag
 from americangut.ag_data import *
 
 
 class AgDataTest(TestCase):
     def setUp(self):
-        d = TempDirectory()
+        self.repo_dir = ag.WORKING_DIR.split('American-Gut')[0]
+        self.base_dir = os.path.join(self.repo_dir,
+                                     'American-Gut/tests/data/11-packaged')
+        self.bodysite = 'fecal'
+        self.trim = '100nt'
+        self.depth = '1k'
+
+        self.ag_data = AgData(
+            bodysite=self.bodysite,
+            trim=self.trim,
+            depth=self.depth,
+            base_dir=self.base_dir
+            )
+
+        data_dir = os.path.join(self.base_dir,
+                                'fecal/100nt/all_participants/all_samples/1k/')
+        known_map = pd.read_csv(os.path.join(data_dir, 'ag_1k_fecal.txt'),
+                                sep='\t',
+                                dtype=str,
+                                na_values=['NA', 'unknown', '', 'no_data',
+                                           'None', 'Unknown'],
+                                ).set_index('#SampleID')
+        alpha_cols = ['PD_whole_tree_1k', 'shannon_1k', 'chao1_1k',
+                      'observed_otus_1k']
+        known_map[alpha_cols] = known_map[alpha_cols].astype(float)
+        self.map_ = known_map
+        self.otu_ = biom.load_table(os.path.join(data_dir, 'ag_1k_fecal.biom'))
+        self.beta = {
+            'unweighted_unifrac': skbio.DistanceMatrix.read(
+                os.path.join(data_dir, 'unweighted_unifrac_ag_1k_fecal.txt')
+                ),
+            'weighted_unifrac': skbio.DistanceMatrix.read(
+                os.path.join(data_dir, 'weighted_unifrac_ag_1k_fecal.txt')
+            )}
 
     def test__init__(self):
-        bodysite = 'fecal'
-        trim = '100nt'
-        depth = '1k'
+        known_data_set = {'bodysite': self.bodysite,
+                          'sequence_trim': self.trim,
+                          'rarefaction_depth': self.depth,
+                          'samples_per_participants': 'all_samples',
+                          'participant_set': 'all'
+                          }
 
         test = AgData(
-            bodysite=bodysite,
-            trim=trim,
-            depth=depth
+            bodysite=self.bodysite,
+            trim=self.trim,
+            depth=self.depth,
+            base_dir=self.base_dir
             )
-        self.assertEqual(test.data_set['bodysite'], bodysite)
-        self.assertEqual(test.data_set['sequence_trim'], trim)
-        self.assertEqual(test.data_set['rarefaction_depth'], depth)
-        self.assertEqual(test.data_set['samples_per_participants'],
-                         'all_samples')
-        self.assertEqual(test.data_set['participant_set'], 'all')
+        # Checks dataset
+        self.assertEqual(test.data_set, known_data_set)
+        self.assertEqual(test.beta, self.beta)
+        pdt.assert_frame_equal(test.map_, self.map_)
+        npt.assert_array_equal(test.otu_.ids('sample'),
+                               self.otu_.ids('sample'))
+        npt.assert_array_equal(test.otu_.ids('observation'),
+                               self.otu_.ids('observation'))
+        for sample in test.otu_.ids('sample'):
+            npt.assert_array_equal(test.otu_.data(sample),
+                                   self.otu_.data(sample))
 
+    def test__init__subset(self):
+        data_dir = os.path.join(self.base_dir,
+                                'fecal/100nt/sub_participants/one_sample/10k/')
+        known_map = pd.read_csv(os.path.join(data_dir, 'ag_10k_fecal.txt'),
+                                sep='\t',
+                                dtype=str,
+                                na_values=['NA', 'unknown', '', 'no_data',
+                                           'None', 'Unknown'],
+                                ).set_index('#SampleID')
+        alpha_cols = ['PD_whole_tree_1k', 'PD_whole_tree_10k',
+                      'shannon_1k', 'shannon_10k',
+                      'chao1_1k', 'chao1_10k',
+                      'observed_otus_1k', 'observed_otus_10k']
+        known_map[alpha_cols] = known_map[alpha_cols].astype(float)
+        known_otu = biom.load_table(os.path.join(data_dir,
+                                    'ag_10k_fecal.biom'))
+        known_beta = {
+            'unweighted_unifrac': skbio.DistanceMatrix.read(
+                os.path.join(data_dir, 'unweighted_unifrac_ag_10k_fecal.txt')
+                ),
+            'weighted_unifrac': skbio.DistanceMatrix.read(
+                os.path.join(data_dir, 'weighted_unifrac_ag_10k_fecal.txt')
+            )}
+        known_data_set = {'bodysite': self.bodysite,
+                          'sequence_trim': self.trim,
+                          'rarefaction_depth': '10k',
+                          'samples_per_participants': 'one_sample',
+                          'participant_set': 'sub'
+                          }
+
+        test = AgData(
+            bodysite=self.bodysite,
+            trim=self.trim,
+            depth='10k',
+            one_sample=True,
+            sub_participants=True,
+            base_dir=self.base_dir
+            )
+        # Checks dataset
+        self.assertEqual(test.data_set, known_data_set)
+        self.assertEqual(test.beta, known_beta)
+        pdt.assert_frame_equal(test.map_, known_map)
+        npt.assert_array_equal(test.otu_.ids('sample'),
+                               known_otu.ids('sample'))
+        npt.assert_array_equal(test.otu_.ids('observation'),
+                               known_otu.ids('observation'))
+        for sample in test.otu_.ids('sample'):
+            npt.assert_array_equal(test.otu_.data(sample),
+                                   known_otu.data(sample))
+
+    def test__init__bodysite(self):
+        with self.assertRaises(ValueError):
+            AgData(bodysite='Vagina',
+                   trim=self.trim,
+                   depth=self.depth)
+
+    def test__init__trim(self):
+        with self.assertRaises(ValueError):
+            AgData(bodysite=self.bodysite,
+                   trim='75nt',
+                   depth=self.depth)
+
+    def test__init__depth(self):
+        with self.assertRaises(ValueError):
+            AgData(bodysite=self.bodysite,
+                   trim=self.trim,
+                   depth='5k')
+
+    def test__init__dir_error(self):
+        no_repo = os.path.join(self.repo_dir, 'LukeCageIsABadass')
+        with self.assertRaises(ValueError):
+            AgData(
+                bodysite=self.bodysite,
+                trim=self.trim,
+                depth=self.depth,
+                base_dir=no_repo,
+                )
+
+    def test_reload_files(self):
+        # Loads the files for the final version
+        data_dir = os.path.join(self.base_dir,
+                                'skin/100nt/all_participants/all_samples/1k/')
+        known_map = pd.read_csv(os.path.join(data_dir, 'ag_1k_skin.txt'),
+                                sep='\t',
+                                dtype=str,
+                                na_values=['NA', 'unknown', '', 'no_data',
+                                           'None', 'Unknown'],
+                                ).set_index('#SampleID')
+        alpha_cols = ['PD_whole_tree_1k', 'shannon_1k', 'chao1_1k',
+                      'observed_otus_1k']
+        known_map[alpha_cols] = known_map[alpha_cols].astype(float)
+        known_otu = biom.load_table(os.path.join(data_dir,
+                                    'ag_1k_skin.biom'))
+        known_beta = {
+            'unweighted_unifrac': skbio.DistanceMatrix.read(
+                os.path.join(data_dir, 'unweighted_unifrac_ag_1k_skin.txt')
+                ),
+            'weighted_unifrac': skbio.DistanceMatrix.read(
+                os.path.join(data_dir, 'weighted_unifrac_ag_1k_skin.txt')
+            )}
+
+        # Tests the fecal files are loaded
+        self.assertEqual(self.ag_data.beta, self.beta)
+        pdt.assert_frame_equal(self.ag_data.map_, self.map_)
+        npt.assert_array_equal(self.ag_data.otu_.ids('sample'),
+                               self.otu_.ids('sample'))
+        npt.assert_array_equal(self.ag_data.otu_.ids('observation'),
+                               self.otu_.ids('observation'))
+        for sample in self.ag_data.otu_.ids('sample'):
+            npt.assert_array_equal(self.ag_data.otu_.data(sample),
+                                   self.otu_.data(sample))
+
+        # Changes the bodysite
+        self.ag_data.bodysite = 'skin'
+        self.ag_data.reload_files()
+
+        # Checks dataset
+        self.assertEqual(self.ag_data.beta, known_beta)
+        pdt.assert_frame_equal(self.ag_data.map_, known_map)
+        npt.assert_array_equal(self.ag_data.otu_.ids('sample'),
+                               known_otu.ids('sample'))
+        npt.assert_array_equal(self.ag_data.otu_.ids('observation'),
+                               known_otu.ids('observation'))
+        for sample in self.ag_data.otu_.ids('sample'):
+            npt.assert_array_equal(self.ag_data.otu_.data(sample),
+                                   known_otu.data(sample))
 
 if __name__ == '__main__':
     main()
